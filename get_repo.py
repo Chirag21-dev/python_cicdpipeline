efe250241c3cbe02cbcd0c63b628af4b78154fb9
Chirag21-dev <chiragks21@outlook.com>
2025-03-21 13:18:39 +0530

    Update get_repo.py

--- get_repo.py
+++ get_repo.py
@@ -1,6 +1,6 @@
 GITHUB_API_URL = "https://api.github.com"
-GITHUB_USERNAME = "Devops-Techstack"  # Replace with your GitHub organization name
-TOKEN = ""  # Replace with your GitHub token
+GITHUB_USERNAME = "Chirag21-dev"  # Replace with your GitHub organization name
+TOKEN = "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"  # Replace with your GitHub token
 USER_TO_REMOVE = "yaswant24"  # User to be removed
 
 #Get all the repository list in your organization
@@ -8,7 +8,7 @@ def get_repositories():
     """Get all repositories in the organization."""
     url = f'https://api.github.com/users/{GITHUB_USERNAME}/repos'
     #Call the REST API 
-    response = requests.get(url, auth=('username', TOKEN))  # Replace 'username' with your GitHub username
+    response = requests.get(url, auth=('Chirag21-dev', TOKEN))  # Replace 'username' with your GitHub username
     #If response code is 200 , your call is success
     if response.status_code == 200:
         return response.json()
